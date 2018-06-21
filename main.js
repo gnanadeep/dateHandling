67324f5fc71195d4bd63eee6b3de94aa33379121
chandu <chandugnanadeep@gmail.com>
2018-06-21 17:16:17 -0400

    final code before edit

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
 function start(){
-    var currnet = new Date(); 
-    document.getElementById("stringPara").innerHTML = 
+    var current = new Date(); 
+    document.getElementById("stringsPara").innerHTML = 
     "<h2>String representation and valueoOf </h2>" + 
-    "<p>toString: " + currnet.toString() + "</p>" + 
-    "<p>toLocaleString: " + currnet.toLocaleString() + "</p>"+
-    "<p>toUTCString: "+ currnet.toUTCString() + "</p>" + 
-    "<p>valueOf: " + current.valueOf() + "</p>";
+    "<p>toString: " + current.toString() + "</p>" + 
+    "<p>toLocaleString: " + current.toLocaleString() + "</p>"+
+    "<p>toUTCString: "+ current.toUTCString() + "</p>" + 
+    "<p>valueOf: " + current.valueOf() + "</p>" ;
 
     document.getElementById("getMethodsPara" ).innerHTML = 
     "<h2> get methods for local time zone</h2>" + 
@@ -19,13 +19,22 @@ function start(){
     "<p>getMinutes: " + current.getMinutes() + "</p>" +
     "<p>getSeconds" + current.getSeconds() + "</p>" +
     "<p>getMilliseconds: " + current.getMilliseconds() + "</p>" +
-    "<p>getTiemzoneOffset: " + current.getTimezoneOffset(); 
+    "<p>getTiemzoneOffset: " + current.getTimezoneOffset() + "</p>" 
     
     var examDate = new Date(2018, 09, 21, 12, 30, 00, 00 );
     document.getElementById("anotherDatePara").innerHTML = 
     "<h2>Specifying arguments for new Date</h2>" + 
-    "<p>Date: " + anotherDate + "</p>";    
+    "<p>Date: "+ anotherDate + "</p>";    
 
+    anotherDate.setDate( 10 );
+    anotherDate.setMonth(11);
+    anotherDate.setFullYear(2016);
+    anotherDate.setHours( 23 );
+    anotherDate.setMinutes( 59 );
+    anotherDate.setSeconds( 59 );
+    document.getElementById( "setMethodsPara").innerHTML = 
+    "<h2> Set methods for local rime zone</h2>" + 
+    "<p>Modified date: " + anotherDate + "</p>";
 }
 
 window.addEventListener("load",start,false);
